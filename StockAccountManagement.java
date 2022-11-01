165cf3047b07df551cc5418159bea848017c2a9f
Mr-Raja12 <rk9576899082@gmail.com>
2022-11-01 12:51:32 +0530

    Printing the Stock Report and modified the class account

--- StockAccountManagement.java
+++
@@ -1,7 +0,0 @@
-package com.bl.stockaccountmanagement;
-public class StockAccountManagement {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Stock Account Management Program");
-    }
-}
-
